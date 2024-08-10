530dba11db69171c94eff42ff5dfc4f0c6fb9a00
mlynetz <romamail2009@gmail.com>
2024-08-10 08:09:33 +0300

    rename project and start app

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'romas_blog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyBlog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
